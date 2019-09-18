458ae3d86072a60eb3e8265f361e4583c47a42da
Kenji <caio_yariwake@hotmail.com>
2019-09-18 16:15:07 -0300

    Obtendo um post por id

---
+++ src/main/java/com/kennji/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.kennji.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kennji.workshopmongo.domain.Post;
+import com.kennji.workshopmongo.repository.PostRepository;
+import com.kennji.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	//Instanciação do obj automatico do Spring
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
